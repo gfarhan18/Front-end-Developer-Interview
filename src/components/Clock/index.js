4c0d1262c5b0d066c4de25c11003350d738ee3b2
Muhammad Farhan <gfarhan18@gmail.com>
2023-12-05 15:48:56 +0500

    working on clock

---
+++ src/components/Clock/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import './clock.css';
+
+const Clock = () => {
+    const [time,setTime] = useState(new Date());
+    console.log(time);
+    useEffect(()=>{
+        const intervalId=setInterval(()=>{
+            setTime(new Date())
+        },1000);
+        return clearInterval(intervalId);
+    },[])
+    const hours=time.getHours%12;
+    const minutes=time.getMinutes();
+    const seconds=time.getSeconds();
+
+    const hourRotation = `rotate(${360*(hours/12)+90+(30*(minutes/60))}deg)`;
+    const minuteRotation = `rotate(${360 * (seconds / 60) + 90}deg)`;
+    const SecondRotation = `rotate(${360 * (seconds / 60) + 90}deg)`;
+
+  return (
+    <div className='clockContainer'>
+
+        {/* <div className="origin-top-left rotate-[51.26deg] w-[27.59px] h-[197.75px] relative">
+        </div> */}
+        <div className='hand hour-hand'></div>
+        
+        <div className='hand minute-hand'></div>
+        <div className='hand second-hand'></div>
+
+    </div>
+  )
+}
+
+export default Clock
